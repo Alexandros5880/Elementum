f95dfe89093304a5122539c6b494f54a6046ed83
Αλέξανδρος Πλατάνιος <alexandrosplatanios15@gmail.com>
2020-11-08 15:32:45 +0200

    fifthteenth image on footer

--- wp-content/themes/twentyseventeen/footer.php
+++ wp-content/themes/twentyseventeen/footer.php
@@ -15,33 +15,23 @@
 ?>
 
 		</div><!-- #content -->
-
 		<footer id="colophon" class="site-footer" role="contentinfo">
-			<div class="wrap">
-				<?php
-				get_template_part( 'template-parts/footer/footer', 'widgets' );
-
-				if ( has_nav_menu( 'social' ) ) :
-					?>
-					<nav class="social-navigation" role="navigation" aria-label="<?php esc_attr_e( 'Footer Social Links Menu', 'twentyseventeen' ); ?>">
-						<?php
-							wp_nav_menu(
-								array(
-									'theme_location' => 'social',
-									'menu_class'     => 'social-links-menu',
-									'depth'          => 1,
-									'link_before'    => '<span class="screen-reader-text">',
-									'link_after'     => '</span>' . twentyseventeen_get_svg( array( 'icon' => 'chain' ) ),
-								)
-							);
-						?>
-					</nav><!-- .social-navigation -->
-					<?php
-				endif;
-
-				get_template_part( 'template-parts/footer/site', 'info' );
-				?>
-			</div><!-- .wrap -->
+			
+			<!--
+			<img src="http://localhost:8080/Elementum/wp-content/uploads/2020/11/elementum333-e1604841259683.jpg" width="100%" alt="Footer Image" />
+			<div class="centered">697 793 4072</div>
+			<div class="centered">Shaori81@yahoo.com</div>
+			-->
+			
+			<div class="container">
+				<img src="http://localhost:8080/Elementum/wp-content/uploads/2020/11/elementum333-e1604841259683.jpg" alt="Norway" style="width:100%;">
+				<div class="text-block">
+					<h4>697 793 4072</h4>
+					<p>Shaori81@yahoo.com</p>
+				</div>
+			</div>
+			
+			
 		</footer><!-- #colophon -->
 	</div><!-- .site-content-contain -->
 </div><!-- #page -->
@@ -49,3 +39,19 @@
 
 </body>
 </html>
+
+
+<style>
+	.container {
+		position: relative;
+	}
+	.text-block {
+		position: absolute;
+		bottom: 100px;
+		right: 20px;
+		background-color: none;
+		color: white;
+		padding-left: 20px;
+		padding-right: 20px;
+	}
+</style>
