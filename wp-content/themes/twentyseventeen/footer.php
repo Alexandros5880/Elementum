a179f447a124ea7f639a22aa9cada094749c5ba0
Αλέξανδρος Πλατάνιος <alexandrosplatanios15@gmail.com>
2020-11-09 02:31:22 +0200

    seventeenth

--- wp-content/themes/twentyseventeen/footer.php
+++ wp-content/themes/twentyseventeen/footer.php
@@ -17,20 +17,20 @@
 		</div><!-- #content -->
 		<footer id="colophon" class="site-footer" role="contentinfo">
 			
-			<!--
-			<img src="http://localhost:8080/Elementum/wp-content/uploads/2020/11/elementum333-e1604841259683.jpg" width="100%" alt="Footer Image" />
-			<div class="centered">697 793 4072</div>
-			<div class="centered">Shaori81@yahoo.com</div>
-			-->
-			
 			<div class="container">
 				<img src="http://localhost:8080/Elementum/wp-content/uploads/2020/11/elementum333-e1604841259683.jpg" alt="Norway" style="width:100%;">
-				<div class="text-block">
+				<div class="text-block-right">
 					<h4>697 793 4072</h4>
 					<p>Shaori81@yahoo.com</p>
 				</div>
+				<div class="text-block-left">  <!-- https://www.w3schools.com/howto/tryit.asp?filename=tryhow_css_social_media_buttons -->
+					<link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/4.7.0/css/font-awesome.min.css">
+					<!-- Add font awesome icons -->
+					<a href="https://www.facebook.com/martial.fitness.glyfada/" class="fa fa-facebook"></a>
+					<a href="https://www.instagram.com/elementum.drago/" class="fa fa-instagram"></a>
+					<a href="https://www.youtube.com/channel/UC2ZFL6YkFLbL8EAODEe4mgQ" class="fa fa-youtube"></a>
+				</div>
 			</div>
-			
 			
 		</footer><!-- #colophon -->
 	</div><!-- .site-content-contain -->
@@ -45,7 +45,7 @@
 	.container {
 		position: relative;
 	}
-	.text-block {
+	.text-block-right {
 		position: absolute;
 		bottom: 100px;
 		right: 20px;
@@ -53,5 +53,42 @@
 		color: white;
 		padding-left: 20px;
 		padding-right: 20px;
+	}
+
+	.text-block-left {
+		position: absolute;
+		bottom: 10px;
+		left: 10px;
+		background-color: none;
+		color: white;
+		padding-left: 10px;
+		padding-right: 10px;
+		/*background: green;*/
+	}
+
+	.fa {
+		padding: 10px;
+		font-size: 30px;
+		width: 50px;
+		text-align: center;
+		text-decoration: none;
+	}
+	.fa:hover {
+		opacity: 0.7;
+	}
+	.fa-facebook {
+		/*background: #3B5998;*/
+		background: none;
+		color: #3B5998 !important;
+	}
+	.fa-instagram {
+		/*background: #125688;*/
+		background: none;
+		color: #E3009E !important;
+	}
+	.fa-youtube {
+		/*background: #bb0000;*/
+		background: none;
+		color: #bb0000 !important;
 	}
 </style>
--- wp-content/themes/twentyseventeen/header.php
+++ wp-content/themes/twentyseventeen/header.php
@@ -17,6 +17,7 @@
 <head>
 <meta charset="<?php bloginfo( 'charset' ); ?>">
 <meta name="viewport" content="width=device-width, initial-scale=1">
+<meta name="keywords" content="Kung Fu, Wing Chun, Fitness, Yoga, Pilates, Martial Arts" />
 <link rel="profile" href="https://gmpg.org/xfn/11">
 
 <?php wp_head(); ?>
